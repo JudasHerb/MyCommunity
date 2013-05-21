b8cf2bb4815f6519927801903c87860bc9702577
JudasHerb <david.garratt.little@gmail.com>
2013-05-21 10:43:23 +0100

    fixed tests

--- MyCommunity.Tests/Controllers/HomeControllerTest.cs
+++ MyCommunity.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace MyCommunity.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("Modify this template to kick-start your ASP.NET MVC application.", result.ViewBag.Message);
         }
 
         [TestMethod]
